9b4d324d70c0c9d87669b059716eccbe34abbba4
Nikhil Choudhary <nikhil0653@gmail.com>
2024-10-20 00:02:16 +0530

    backend

---
+++ Backend/src/index.ts
@@ -0,0 +1,24 @@
+import { Hono } from "hono";
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    // we did this because typescript does not understand what c.env.DATABASE_URL is
+    // and this is how we tell it that it is of string datatype and hence we can now successfull injects the
+    // database url in the index.ts file
+    JWT_SECRET: string;
+  }
+}>();
+
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+//we need file based routing for the app
+
+export default app;
+
+//but we also need prisma to create a connection pool to access the database
+//since we are creting a serverless application so it will create multiple instances of an application around the world
+//it is wrong approach when multiple mini machines connect to db it is good practice to connect to a connection pool first.
--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -2,22 +2,18 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from "hono/jwt";
+import { signupInput } from "../zod";
 
-const app = new Hono<{
+export const userRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
-    // we did this because typescript does not understand what c.env.DATABASE_URL is
-    // and this is how we tell it that it is of string datatype and hence we can now successfull injects the
-    // database url in the index.ts file
     JWT_SECRET: string;
   };
 }>();
 
-//we need file based routing for the app
-
 // SIGNUP Route
 
-app.post("/api/v1/user/signup", async (c) => {
+userRouter.post("/signup", async (c) => {
   const body = await c.req.json();
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL, // the env variable is not accessible globally it must happen in each and every route
@@ -42,14 +38,21 @@ app.post("/api/v1/user/signup", async (c) => {
   } catch (e) {
     console.log(e);
     c.status(403);
-    return c.text('Invalid');
+    return c.text("Invalid");
   }
-})
+});
 
 // SIGNIN Route
 
-app.post("/api/v1/user/signin", async (c) => {
+userRouter.post("/signin", async (c) => {
   const body = await c.req.json();
+
+  const {success} = signupInput.safeParse(body);
+    if(!success){
+      c.status(411);
+      return c.json({message: "Inputs not correct"});
+    }
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL, // the env variable is not accessible globally it must happen in each and every route
   }).$extends(withAccelerate());
@@ -59,13 +62,14 @@ app.post("/api/v1/user/signin", async (c) => {
       where: {
         username: body.username,
         password: body.password,
-        name: body.name,
       },
     });
     if (!user) {
       // if user dosent exist
       c.status(403); // ststus code for unauthorized
-      return c.text("Invalid");
+      return c.json({
+        messgae: "Incorrect creds",
+      });
     }
     const jwt = await sign(
       {
@@ -78,28 +82,6 @@ app.post("/api/v1/user/signin", async (c) => {
   } catch (e) {
     console.log(e);
     c.status(411);
-    return c.text('Invalid');
+    return c.text("Invalid");
   }
-})
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
 });
-
-app.put("/api/v1/user/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/api/v1/user/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/api/v1/user/blog/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-export default app;
-
-//but we also need prisma to create a connection pool to access the database
-//since we are creting a serverless application so it will create multiple instances of an application around the world
-//it is wrong approach when multiple mini machines connect to db it is good practice to connect to a connection pool first.
